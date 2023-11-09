c1c810bd8bc083570837af8629df2997df9814ff
evansochieng <evansochieng121@gmail.com>
2023-11-10 01:23:39 +0300

    partially crack parsing emails

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,17 @@
-# your code goes here!
+# create EmailAddressParser class
+
+# import the re module
+import re
+class EmailAddressParser:
+    
+    # initialize an instance with a string of emails
+    def __init__(self, email_string):
+        self.email_string = email_string
+        
+    # create a parse()
+    def parse(self):
+        emails = re.split(r"[\s,]+", self.email_string)
+        # sort the emails
+        emails.sort()
+        return emails
+        
